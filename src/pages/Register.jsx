fb846b6e335102bc6841674d699058945474826e
Sudeep Mukherjee <sudeep.mukherjee@solvezy.com>
2023-02-12 18:33:45 +0530

    Added design for register page

---
+++ src/pages/Register.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div className="formContainer">
+        <div className="formWrapper">
+            <span className="logo">Tarang Chat</span>
+            <span className="title">Register</span>
+            <form>
+                <input type="text" placeholder="display name" />
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <input type="file" />
+                <button>Sign up</button>
+            </form>
+            <p>You do have an account? Login</p>
+        </div>
+    </div>
+  )
+}
